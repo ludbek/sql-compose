d7a8bd348300c3310abfb4770165422cd5bab7a5
ludbek <sth.srn@gmail.com>
2020-11-04 11:45:26 +1100

    updated project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sql-compose "0.1.0"
   :description "A simple sql query composer for clojure."
-  :url "https://github.com/ludbek/sql-query"
+  :url "https://github.com/ludbek/sql-compose"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
